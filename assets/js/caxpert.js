0b7eb6f61839f108edf4ebfaf5b63e107b9673df
mitul kalsariya <73096168+mitulniqox@users.noreply.github.com>
2024-02-10 09:20:39 +0530

    project add

---
+++ assets/js/caxpert.js
@@ -0,0 +1,34 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+window.visitorGlobalObject=window.visitorGlobalObject||window.prismGlobalObject;window.visitorGlobalObject.setVisitorId('65137dc3-b2ac-4156-bfe2-53c68297a863', '476326407');window.visitorGlobalObject.setWhitelistedServices('tracking', '476326407');
+
+}
+/*
+     FILE ARCHIVED ON 09:25:44 Feb 14, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 11:05:44 Feb 09, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots: 0.097
+  exclusion.robots.policy: 0.089
+  cdx.remote: 0.098
+  esindex: 0.01
+  LoadShardBlock: 113.688 (6)
+  PetaboxLoader3.datanode: 128.371 (7)
+  load_resource: 109.342
+  PetaboxLoader3.resolve: 47.513
+*/
