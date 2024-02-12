291720458076cbabdd804cc897817b9724a6bbb0
mitul kalsariya <73096168+mitulniqox@users.noreply.github.com>
2024-02-12 13:49:49 +0530

    new project add

--- assets/js/style.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 1) {
-        $('.navbar').addClass("sticky_tops");
-    }
-    else {
-        $('.navbar').removeClass("sticky_tops");
-    }
-});
